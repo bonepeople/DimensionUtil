3b4fcc6ca40114ec7843864fca063d956d9b650e
bonepeople <bonepeople@sina.com>
2019-12-11 10:12:56 +0800

    添加工具类及示例界面

---
+++ library/src/main/java/com/bonepeople/android/lib/dimensionutil/DimensionUtil.java
@@ -0,0 +1,69 @@
+package com.bonepeople.android.lib.dimensionutil;
+
+import android.content.res.Resources;
+import android.util.TypedValue;
+
+/**
+ * 尺寸转换工具类
+ * <p>
+ * 包含通用的尺寸转换方法
+ *
+ * @author bonepeople
+ */
+public class DimensionUtil {
+
+    /**
+     * 获取px单位数值
+     * <p>
+     * 将dp单位的数值转换为px单位的数值，如果转换单位不是dp，可以调用{@link #getPx(int, float)}进行转换
+     *
+     * @param dp 被转换数值，单位为dp
+     * @return 转换后的数值，单位为px
+     */
+    public static int getPx(float dp) {
+        return getPx(TypedValue.COMPLEX_UNIT_DIP, dp);
+    }
+
+    /**
+     * 获取px单位数值
+     * <p>
+     * 将指定单位的数值转换为px单位的数值
+     *
+     * @param unit  转换单位，参照{@link android.util.TypedValue}的单位定义
+     * @param value 被转换数值
+     * @return 转换后的数值，单位为px
+     */
+    public static int getPx(int unit, float value) {
+        return (int) TypedValue.applyDimension(unit, value, Resources.getSystem().getDisplayMetrics());
+    }
+
+    /**
+     * 获取dp单位数值
+     * <p>
+     * 将px单位的数值转换为dp单位的数值
+     *
+     * @param px 被转换数值，单位为px
+     * @return 转换后的数值，单位为dp
+     */
+    public static float getDp(int px) {
+        return px / Resources.getSystem().getDisplayMetrics().density;
+    }
+
+    /**
+     * 获取屏幕显示区域的宽度
+     *
+     * @return 屏幕宽度，单位为px
+     */
+    public static int getDisplayWidth() {
+        return Resources.getSystem().getDisplayMetrics().widthPixels;
+    }
+
+    /**
+     * 获取屏幕显示区域的高度
+     *
+     * @return 屏幕高度，单位为px
+     */
+    public static int getDisplayHeight() {
+        return Resources.getSystem().getDisplayMetrics().heightPixels;
+    }
+}
--- simple/src/main/java/com/bonepeople/android/dimensionutil/simple/MainActivity.java
+++ simple/src/main/java/com/bonepeople/android/dimensionutil/simple/MainActivity.java
@@ -2,12 +2,75 @@ package com.bonepeople.android.dimensionutil.simple;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.bonepeople.android.lib.dimensionutil.DimensionUtil;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView textView_display, textView_1, textView_2, textView_3;
+    private EditText editText_1, editText_2, editText_3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView_display = findViewById(R.id.textView_display);
+        textView_1 = findViewById(R.id.textView_1);
+        textView_2 = findViewById(R.id.textView_2);
+        textView_3 = findViewById(R.id.textView_3);
+        editText_1 = findViewById(R.id.editText_1);
+        editText_2 = findViewById(R.id.editText_2);
+        editText_3 = findViewById(R.id.editText_3);
+        findViewById(R.id.button_1).setOnClickListener(this);
+        findViewById(R.id.button_2).setOnClickListener(this);
+        findViewById(R.id.button_3).setOnClickListener(this);
+
+        textView_display.setText("当前手机的屏幕宽度为:" + DimensionUtil.getDisplayWidth() + "，高度为:" + DimensionUtil.getDisplayHeight());
+    }
+
+    private void dp2px() {
+        try {
+            float dp = Float.parseFloat(editText_1.getText().toString());
+            textView_1.setText(String.valueOf(DimensionUtil.getPx(dp)));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void px2dp() {
+        try {
+            int px = Integer.parseInt(editText_2.getText().toString());
+            textView_2.setText(String.valueOf(DimensionUtil.getDp(px)));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void sp2px() {
+        try {
+            float sp = Float.parseFloat(editText_3.getText().toString());
+            textView_3.setText(String.valueOf(DimensionUtil.getPx(TypedValue.COMPLEX_UNIT_SP, sp)));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_1:
+                dp2px();
+                break;
+            case R.id.button_2:
+                px2dp();
+                break;
+            case R.id.button_3:
+                sp2px();
+                break;
+        }
     }
 }
